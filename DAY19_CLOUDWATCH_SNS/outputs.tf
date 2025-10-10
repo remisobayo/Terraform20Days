403c3b12c38723f65807c444a05d552147952a7a
Remi Sobayo <remis@anmutetechnologies.com>
2025-10-10 11:57:18 -0500

    Added Day19 code

--- DAY19_CLOUDWATCH_SNS/outputs.tf
+++ DAY19_CLOUDWATCH_SNS/outputs.tf
@@ -2,10 +2,10 @@
 
 output "KKE_sns_topic_name" {
     description = "for the SNS topic name."
-    value = ""
+    value = aws_sns_topic.sns_topic.name
 }
 
 output "KKE_cloudwatch_alarm_name" {
     description = "for the CloudWatch alarm name."
-    value = ""
+    value = aws_cloudwatch_metric_alarm.compute_alarm.alarm_name
     }
